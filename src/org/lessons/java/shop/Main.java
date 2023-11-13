f9cae86c930a89ca6cb9e2d46c7e67209a5bf2f1
AntonelloStefania <antonelloste94@gmail.com>
2023-11-13 16:32:47 +0100

    finito esercizio base

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,43 @@
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+public class Main {
+	public static void main(String[] args) {
+		final int IVA = 21;
+		final int PRODUCT_COUNT=10;
+		
+		String[] techProducts = {
+		            "Smartphone",
+		            "Laptop",
+		            "Tablet",
+		            "Smart TV",
+		            "Game Console",
+		            "Smartwatch",
+		            "Headphones",
+		            "Router",
+		            "Digital Camera",
+		            "Fitness Tracker"
+		        };
+		 
+		 String[] techProductDescriptions = {
+		            "Un dispositivo avanzato per la comunicazione mobile.",
+		            "Un computer portatile altamente efficiente e leggero.",
+		            "Una tavoletta elettronica per la navigazione web e l'intrattenimento.",
+		            "Una TV intelligente con accesso a una vasta gamma di contenuti.",
+		            "Una console di gioco per divertimento e intrattenimento.",
+		            "Un orologio intelligente con molte funzionalità pratiche.",
+		            "Audio di alta qualità per un'esperienza audio eccezionale.",
+		            "Un dispositivo per la connessione a Internet wireless.",
+		            "Una fotocamera digitale per catturare momenti speciali.",
+		            "Un braccialetto per il monitoraggio dell'attività fisica."
+		        };
+		
+		Random rnd = new Random();
+		
+		for(int x=0; x<PRODUCT_COUNT; x++) {
+			Prodotto p = new Prodotto(rnd.nextInt(Integer.MAX_VALUE),techProducts[rnd.nextInt(techProducts.length)], techProductDescriptions[rnd.nextInt(techProductDescriptions.length)],rnd.nextFloat(0,299),IVA);
+			System.out.println(p);
+		}
+	}
+}
---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,66 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+	private int code;
+	private String name;
+	private String description;
+	private float price;
+	private int iva;
+	
+	public Prodotto(int code, String name, String description, float price, int iva) {
+		setCode(code);
+		setName(name);
+		setDescription(description);
+		setPrice(price);
+		setIva(iva);
+		
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	private void setCode(int code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getIva() {
+		return iva;
+	}
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
+	
+	public float getFullPrice() {
+		return getPrice() * (100 + iva) / 100;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "Prodotto :\n"
+				+ "code: " + getCode() + "\n"
+				+ "nome: " + getName() + "\n"
+				+ "descrizione: " + getDescription() + "\n"
+				+ "prezzo: " + String.format("%.02f", getPrice()) + "€\n"
+				+ "iva: " + getIva() + "%\n"
+				+ "il prezzo comprensivo di iva e': " + String.format("%.02f", getFullPrice()) +"€\n\n";
+	}
+	
+}
